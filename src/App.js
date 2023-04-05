575381b7d13375523871dea79f48c52b9a22d2db
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-04-05 17:52:59 +0400

    feature/navbar-add-css/and/responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './app.css';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Main from './components/Main/Main';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <h2>
- App":lklkjghkjdfdsasA
+    <>
+    <Navbar/>
+    {/* <Home/>
+    <Main/>
+    <Footer/> */}
 
-    </h2>
+    </>
   );
 }
 
