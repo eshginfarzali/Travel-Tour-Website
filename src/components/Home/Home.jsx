9c567f1abdaa376bd45ab6ba3e241145b79808c2
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-04-05 19:07:48 +0400

    feature/add-assests-folder

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,11 @@ import "./home.css"
 
 const Home = () => {
   return (
-    <div>home</div>
+    <section className='home'>
+      <div className="overlay"></div>
+      <video src=""></video>
+
+    </section>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,60 +1,78 @@
-import React from 'react'
-import "./navbar.css"
-import {MdOutlineTravelExplore } from "react-icons/md";
-import {AiFillCloseCircle } from "react-icons/ai";
-import {TbGridDots } from "react-icons/tb";
+import React, { useState } from "react";
+import "./navbar.css";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbGridDots } from "react-icons/tb";
 
 const Navbar = () => {
+  const [active, setActive] = useState("navBar");
+  // Function to toggle navBar
+  const showNav = () => {
+    setActive("navBar activeNavbar");
+  };
+  // Function to remove navBar
+  const removeNavbar = () => {
+    setActive("navBar ");
+  };
+
   return (
-  <section className='navBarSection'>
-    <header className="header flex">
-      <div className="logoDiv">
-        <a href="#" className='logo flex'>
-          <h1><MdOutlineTravelExplore
-          className="icon"/> Travel.</h1>
-        </a>
-      </div>
-
-      <div className="navBar">
-        <ul className="navLists flex">
-        <li className="navItem">
-          <a href="#" className="navLink">Home</a>
-        </li>
-        <li className="navItem">
-          <a href="#" className="navLink">Packages</a>
-        </li>
-        <li className="navItem">
-          <a href="#" className="navLink">Shop</a>
-        </li>
-        <li className="navItem">
-          <a href="#" className="navLink">About</a>
-        </li>
-        <li className="navItem">
-          <a href="#" className="navLink">Pages</a>
-        </li>
-        <li className="navItem">
-          <a href="#" className="navLink">News</a>
-        </li>
-        <button className='btn'>
-          <a href="#">BOOK NOW</a>
-        </button>
-        </ul>
-        <div className="closeNavbar">
-          <AiFillCloseCircle className="icon"/>
-
+    <section className="navBarSection">
+      <header className="header flex">
+        <div className="logoDiv">
+          <a href="#" className="logo flex">
+            <h1>
+              <MdOutlineTravelExplore className="icon" /> Travel.
+            </h1>
+          </a>
         </div>
-      </div>
 
-    <div className="toggleNavbar">
-      <TbGridDots className='icon'/>
-    </div>
+        <div className={active}>
+          <ul className="navLists flex">
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Home
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Packages
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Shop
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                About
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Pages
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                News
+              </a>
+            </li>
+            <button className="btn">
+              <a href="#">BOOK NOW</a>
+            </button>
+          </ul>
+          <div onClick={removeNavbar} className="closeNavbar">
+            <AiFillCloseCircle className="icon" />
+          </div>
+        </div>
 
+        <div onClick={showNav} className="toggleNavbar">
+          <TbGridDots className="icon" />
+        </div>
+      </header>
+    </section>
+  );
+};
 
-
-    </header>
-
-  </section>
-  )
-}
-
-export default Navbar
+export default Navbar;
